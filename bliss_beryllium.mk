107be2d9acd2f7ef1eb9daaa66df39325020b57c
EvilVenom <ktanay98@gmail.com>
2020-07-17 08:49:10 +0000

    beryllium: Add missing flags

--- bliss_beryllium.mk
+++ bliss_beryllium.mk
@@ -8,6 +8,10 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
 # Inherit some common bliss stuff.
 $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_beryllium
