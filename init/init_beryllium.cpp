c5e30d43ca59781ed9e245e818d48bb7211f8148
EvilVenøm <ktanay98@gmail.com>
2021-05-09 03:29:50 -0400

    beryllium: Update fingerprint to Redfin May Security Patch

--- init/init_beryllium.cpp
+++ init/init_beryllium.cpp
@@ -68,6 +68,6 @@ void vendor_load_properties()
     };
 
     for (const auto &source : ro_props_default_source_order) {
-        set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys");
+        set_ro_build_prop(source, "fingerprint", "google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys");
         }
 }
